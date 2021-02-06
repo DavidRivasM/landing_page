ba31f16683b58a95015911f4f327ff9896668877
DavidRivasM <alonsorivasmora@gmail.com>
2021-02-06 05:26:37 +0000

    first commit

---
+++ src/js/component/cards.js
@@ -0,0 +1,110 @@
+import React from "react";
+
+export function Cards() {
+	return (
+		<div className="container text-center">
+			<div className="row">
+				<div className="col-3">
+					<div className="card">
+						<img
+							src="http://c.files.bbci.co.uk/48DD/production/_107435681_perro1.jpg"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">
+								Some quick example text to build on the card
+								title and make up the bulk of the card's
+								content.
+							</p>
+							<a href="#" className="btn btn-primary">
+								Go somewhere
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className="col-3">
+					<div className="card">
+						<img
+							src="http://c.files.bbci.co.uk/48DD/production/_107435681_perro1.jpg"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">
+								Some quick example text to build on the card
+								title and make up the bulk of the card's
+								content.
+							</p>
+							<a href="#" className="btn btn-primary">
+								Go somewhere
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className="col-3">
+					<div className="card">
+						<img
+							src="http://c.files.bbci.co.uk/48DD/production/_107435681_perro1.jpg"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">
+								Some quick example text to build on the card
+								title and make up the bulk of the card's
+								content.
+							</p>
+							<a href="#" className="btn btn-primary">
+								Go somewhere
+							</a>
+						</div>
+					</div>
+				</div>
+				<div className="col-3">
+					<div className="card">
+						<img
+							src="http://c.files.bbci.co.uk/48DD/production/_107435681_perro1.jpg"
+							className="card-img-top"
+							alt="..."
+						/>
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">
+								Some quick example text to build on the card
+								title and make up the bulk of the card's
+								content.
+							</p>
+							<a href="#" className="btn btn-primary">
+								Go somewhere
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+{
+	/* <div className="container">
+			<div className="row">
+				<div className="col-3">
+
+                </div>
+       
+				<div className="col-3">
+
+                </div>
+          
+				<div className="col-3">
+
+                </div>
+   
+				<div className="col-3">
+
+                </div> */
+}
---
+++ src/js/component/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Footer() {
+	return (
+		<footer className="footer mt-auto py-3 bg-dark text-white text-center">
+			{" "}
+			<div className="container">
+				{" "}
+				<span>Place sticky footer content here.</span>{" "}
+			</div>{" "}
+		</footer>
+	);
+}
